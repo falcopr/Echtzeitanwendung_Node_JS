9b232d758e65a035947ec8effefb69da69af0fd3
Falco Prescher <falcoprescher@googlemail.com>
2016-11-02 02:55:59 +0100

    Implement room leave/join

--- simplechatrooms/server/index.js
+++ simplechatrooms/server/index.js
@@ -11,28 +11,66 @@ console.log(`Listening on *:${port}`);
 chatNamespace.on(
   'connect',
   function clientConnected(socket) {
-    console.log(`The client ${socket.id} connected`);
+    let currentRoomName = '#root';
+    socket.join(currentRoomName);
+
+    console.log(`The client ${socket.id} connected and joined room ${currentRoomName}`);
     socket.emit('client:getuserid', { socketid: socket.id });
 
-    socket.broadcast.emit(
+    socket.broadcast.to(currentRoomName).emit(
       'server:receivemsg',
       {
         userid: 'Server',
-        msg: `Client ${socket.id} joined the chat.`
+        msg: `Client ${socket.id} joined the chat in room ${currentRoomName}.`
       });
 
     socket.on(
       'client:sendmsg',
       function receivedClientMsg(msg) {
         console.log(`${socket.id}: ${msg}`);
-        socket.broadcast.emit('server:receivemsg', { msg: msg, userid: socket.id });
+        socket.broadcast.to(currentRoomName).emit('server:receivemsg', { msg: msg, userid: socket.id });
+      });
+
+    socket.on(
+      'client:joinroom',
+      function requestRoomJoining(data) {
+        console.log(`${socket.id}: Leaving room ${currentRoomName}`);
+        socket.emit(
+          'server:receivemsg',
+          {
+            userid: 'Server',
+            msg: `You left the room ${currentRoomName}.`
+          });
+        socket.broadcast.to(currentRoomName).emit(
+          'server:receivemsg',
+          {
+            userid: 'Server',
+            msg: `Client ${socket.id} left the room ${currentRoomName}.`
+          });
+        socket.leave(currentRoomName);
+
+        currentRoomName = data.room;
+        console.log(`${socket.id}: Joining room ${currentRoomName}`);
+        socket.join(currentRoomName);
+        socket.emit(
+          'server:receivemsg',
+          {
+            userid: 'Server',
+            msg: `You joined the room ${currentRoomName}.`
+          });
+        socket.broadcast.to(currentRoomName).emit(
+          'server:receivemsg',
+          {
+            userid: 'Server',
+            msg: `Client ${socket.id} joined the room ${currentRoomName}.`
+          });
       });
 
     socket.on(
       'disconnect',
       function clientDisconnected() {
         console.log(`The client ${socket.id} disconnected`);
-        socket.broadcast.emit(
+        socket.broadcast.to(currentRoomName).emit(
           'server:receivemsg',
           {
             userid: 'Server',
